e58b9063ea8baad2df121d02ac68afa6a6e5fd35
clemens kremer <molkrich@gmx.de>
2024-05-15 09:53:57 +0000

    arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,17 +5,44 @@
 
 // Vanilla JavaScript Function
 
-function addTwoNumbers(a, b) {
+function addTwoNumbers (a, b) {
     return a + b;
 };
 
 let sum = addTwoNumbers(3, 5);
-console.log(sum);
+console.log(sum); 
 
 // Arrow Function With Parameters
 
+/** 
+const addTwoNumbers = (a, b) => {
+    return a + b;
+};
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);*/
+
 // Single Line Arrow Function With Parameters
+
+const addTwoNumbers2 = (a, b) => a + b; 
+
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum2);
+
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething ('Hello there!!')
+
+const sayHello = () => console.log('Hello');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+`<p> 
+This is a multiline string! 
+</p>`
+)
+console.log(returnMultipleLines());
